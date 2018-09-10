1cbe8479fca8337c84d570842043990237e2526e
Issam <issam.elhaddar@student.rocva.nl>
2018-09-10 14:43:01 +0200

    Zamel

--- Pages/home.php
+++ Pages/home.php
@@ -3,7 +3,15 @@
 App::pageAuth(['user'], "login");
 
 // Example to get user
-$user = User::findById(1);
+// $user = User::findById(1);
 
 ?>
 
+<table style="margin:0px" class="table table-striped">
+	<thead>
+		<tr>
+	        <th>Materiaal</th>
+	        <th>Aantal</th>
+	    </tr>
+	</thead>
+</table>
